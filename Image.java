fb1abff5cfbdc4f00074121bd11dc5f0817283aa
Peter bock <bockpeter@hotmail.com>
2017-02-06 17:46:03 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,6 +1,10 @@
 public class Image extends Test {
     int nb_echecs;
     boolean reussite;
+    
+    public Image(){
+    nom = "Image";
+    }
 
     public void setReussite(boolean value){reussite = value;}
     public void setNbEchecs(int nb){nb_echecs = nb;}
