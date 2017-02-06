94bceabeb90022227ca29590b29fc6eff3285d22
Peter bock <bockpeter@hotmail.com>
2017-02-06 17:39:45 +0100

    Update Nourriture.java
    added constructor

--- Nourriture.java
+++ Nourriture.java
@@ -1,6 +1,9 @@
 public class Nourriture extends Test {
     int nb_echecs;
 
+    Nourriture(){
+    nom = "Nourriture";
+    }
     public void setNbEchecs(int nb){nb_echecs = nb;}
 
     public void setPopVisee(String pop){pop_visee = pop;}
