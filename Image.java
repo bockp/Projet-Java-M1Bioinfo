41965564d7ae3cd11864d72f4ec5125549cb3c54
bockp <bockpeter@hotmail.com>
2017-02-06 16:07:08 +0100

    Finished creating skeletons for all Test classes

---
+++ Image.java
@@ -0,0 +1,13 @@
+public class Image extends Test {
+    int nb_echecs;
+    boolean reussite;
+
+    public void setReussite(boolean value){reussite = value;}
+    public void setNbEchecs(int nb){nb_echecs = nb;}
+
+    public void setPopVisee(String pop){pop_visee = pop;}
+    public void setGroupeVisee(int groupe){groupe_visee = groupe;}
+
+
+
+}
---
+++ Nourriture.java
@@ -0,0 +1,7 @@
+public class Nourriture extends Test {
+    int nb_echecs;
+
+    public void setNbEchecs(int nb){nb_echecs = nb;}
+
+    public void setPopVisee(String pop){pop_visee = pop;}
+}
