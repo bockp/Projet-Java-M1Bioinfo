bdcb61f968726d06c5638c2f58bf7dcb0be6a73c
bockp <bockpeter@hotmail.com>
2017-02-06 14:52:41 +0100

    Made the basic Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+public abstract class Test{
+    private String nom;
+    private String pop_visee;
+    private int groupe_visee;
+
+    public void setPopVisee(String pop){pop_visee = pop;}
+
+    public void setGroupeVisee(int groupe){groupe_visee = groupe;}
+
+}
