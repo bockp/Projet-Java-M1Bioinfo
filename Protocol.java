aabed7d5c808276d9b990fe0333f35ee2c7f0a3a
Peter bock <bockpeter@hotmail.com>
2017-02-08 17:15:30 +0100

    Update Protocol.java
    good god concurrent edits are a paaaaain.

--- Protocol.java
+++ Protocol.java
@@ -1,5 +1,33 @@
-public class Protocol {
-    
+import java.util.*;
+
+public class Protocol extends ArrayList{
+    List<String> semaine = Arrays.asList("Lundi","Mardi","Mercredi","Jeudi","Vendredi");
+
+    // objets de bases pour chaque test dans le protocol, permettant l'access
+    // au conditions d'applications des tests.
+    Labyrinthe lab = new Labyrinthe();
+    Image img = new Image();
+    Nourriture nourri = new Nourriture();
+
+    List<Test> protocol = Arrays.asList(lab,img,nourri);
 
 
+    public void experience(ArrayList animaux){
+	String jour;
+	int indexJournalier;
+	for(Iterator<String> e = semaine.iterator();e.hasNext();){
+	    jour = e.next();
+	    indexJournalier = semaine.indexOf(jour);
+	    for(Iterator<Animal> anm = animaux.iterator();anm.hasNext();){
+		Animal animal = anm.next();
+		int animalId = anm.getId();
+		System.out.println("Donnez le poids de l'animal:");
+		if(semaine[0] == jour){
+		    animal.setPoidsDebutSemaine();
+		}
+		
+		
+	    }
+	}
+    }
 }
