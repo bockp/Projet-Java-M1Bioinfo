2d9dd12db06aa3c3fbc02a4524d70320cffa7094
bockp <bockpeter@hotmail.com>
2017-02-10 16:37:27 +0100

    added  some stuff

--- Singe.java
+++ Singe.java
@@ -1,16 +1,18 @@
 public class Singe extends Animal
 {
-    private final String espece = "Singe";
+    private final String espece1 = "Singe";
     
     public Singe(String sexe, float poids)
     {
 	super(sexe, poids);
-	this.pop=this.espece;
+	this.pop=this.espece1;
+	this.espece = espece1;
+	
     }
 
     public void afficher_infos()
     {
-	System.out.println("Espece: "+espece);
+	System.out.println("Espece: "+espece1);
 	super.afficher_infos();
     }
 
--- Souris.java
+++ Souris.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class Souris extends Animal
 {
     private final int groupe;
@@ -17,6 +18,25 @@ public class Souris extends Animal
     {
     	super.afficher_infos();
 	System.out.println("Groupe: "+groupe);
+    }
+
+    public void sauvegarder(String filename)
+    {
+	try{
+	    BufferedWriter buff = new BufferedWriter(new FileWriter(filename, true));
+	    buff.write("Id: "+ id + "\n");
+	    buff.write("Espece: "+ espece + "\n");
+	    buff.write("groupe: "+ groupe + "\n");
+	    buff.write("Sexe: "+ sexe + "\n");
+	    buff.write("Poids: "+ poids + "\n");
+	    buff.write("Statut: "+ statut + "\n");
+	    buff.write("Progression: "+ progression + "\n");
+	    buff.write("Etat: "+ etat + "\n");
+	    buff.write("Jour de la meilleure performance: "+ meilleure_performance + "\n");
+	    buff.newLine();
+	    buff.close();
+	}
+	catch(IOException e) {System.out.println("Erreur de sauvegarde: "+ e);}
     }
 }
     
